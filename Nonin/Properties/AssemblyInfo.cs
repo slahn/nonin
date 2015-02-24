d60030dc5e9a24153f29778ab94f2164a2ecb321
Sigmund Lahn <sigmund@lahn.no>
2015-02-24 09:19:05 +0100

    Adding Nuspec

--- Nonin/Properties/AssemblyInfo.cs
+++ Nonin/Properties/AssemblyInfo.cs
@@ -3,23 +3,21 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+
 [assembly: AssemblyTitle("Nonin")]
-[assembly: AssemblyDescription("Parsing and validation of norwegian identity numbers")]
+[assembly: AssemblyDescription("Parsing and validation of norwegian identity numbers (fødselsnummer/personnummer)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sigmund L. Lahn")]
 [assembly: AssemblyProduct("Nonin")]
 [assembly: AssemblyCopyright("Copyright © Sigmund L. Lahn 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("420b3517-de5d-4586-b7a8-e607b72b743f")]
 
@@ -33,7 +31,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: CLSCompliant(true)]
